a3c66c226c44a5aa4ccba562710085ed99e009e7
shafqatrasool13000 <shafqatrasool13000@gmail.com>
2023-01-16 11:47:15 +0500

    review-done

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -3,7 +3,63 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+
+  const checkIndex = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1
+
+    }
+    return number
+  }
+  const addQuotesHandler = () => {
+    setIndex(prev => {
+      let newIndex = prev + 1;
+      return checkIndex(newIndex);
+    });
+  }
+  const removeQuotesHandler = () => {
+    setIndex(prev => {
+      let newIndex = prev - 1;
+      return checkIndex(newIndex);
+    });
+  }
+  console.log({ index })
+  const randomQuoteGenerator = () => {
+    let surpriseQuote = Math.floor(Math.random() * people.length);
+    if (surpriseQuote === index) {
+      surpriseQuote = index + 1;
+    }
+    setIndex(checkIndex(surpriseQuote));
+  }
+  console.log({ index })
+  const { name, image, text, job } = people[index];
+  return <article className='review'>
+    <div className="img-container">
+      <img className='person-img' src={image} alt={name} />
+      <span className="quote-icon">
+        <FaQuoteRight />
+      </span>
+    </div>
+    <h4 className="author">{name}</h4>
+    <p className="job">{job}</p>
+    <p className="info">{text}</p>
+    <div className="button-container">
+      <button className="prev-btn">
+        <FaChevronLeft onClick={() => addQuotesHandler()} />
+      </button>
+      <button className="next-btn">
+        <FaChevronRight onClick={() => removeQuotesHandler()} />
+      </button>
+    </div>
+    <button className='random-btn' onClick={() => randomQuoteGenerator()}>
+      surprise
+    </button>
+
+  </article>;
 };
 
 export default Review;
